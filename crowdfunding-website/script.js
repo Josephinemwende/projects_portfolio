54777bb80862c70b55f6efc4404b8f91629dda4f
Josephine Mwende <Josymwesh45@gmail.com>
2024-07-10 08:06:57 +0300

    Crowdfunding website

---
+++ crowdfunding-website/script.js
@@ -0,0 +1,9 @@
+function handleClick(){
+    let menuIcon = document.getElementById('menu');
+    menuIcon.innerText = `
+        <ul>
+            <li>About</li>
+            <li>Discover</li>
+            <li>Get Started</li>
+        </ul>`;
+}
